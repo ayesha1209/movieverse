e2d03557ea5afd437cc993fbe3c17a3135e5d93b
ayesha1209 <116152639+ayesha1209@users.noreply.github.com>
2023-10-20 18:11:16 +0530

    Add files via upload

--- Scene2Controller.java
+++ Scene2Controller.java
@@ -1,40 +1,63 @@
 package application;
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-public class Scene2Controller {
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-    
+public class Scene2Controller {
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Button JAWAN;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
-    @FXML
-	Label nameLabel;
-	ImageView movie1;
-	 
-	    public void displayName(String username) {
-	    	nameLabel.setText("Welcome: " + username +" to MOVIEVERSE your companion for booking");
-	    }
-	
 
-	    
+    public void displayName(String username) {
+        nameLabel.setText("Welcome: " + username + " to MOVIEVERSE, your companion for booking");
+    }
+
     @FXML
     public void buyTicket(ActionEvent event) throws IOException {
+        // Identify which button was clicked (in this case, we have JAWAN as an example)
+        Button clickedButton = (Button) event.getSource();
+        String buttonId = clickedButton.getId();
+
+        // Insert the movie data into the database
+        insertMovieIntoDatabase(buttonId);
+        
+        try {
+        	 FXMLLoader loader2 = new FXMLLoader(getClass().getResource("Scene4.fxml"));
+			root = loader2.load();
+			Scene4Controller scene = loader2.getController();
+			scene.moviename.setText(buttonId);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        // Load the next scene (Scene3.fxml)
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene3.fxml"));
         root = loader.load();
 
+        // You can pass data to Scene3 if needed
         Scene3Controller scene3Controller = loader.getController();
-        // Pass any necessary data to Scene3 if needed
-        // scene3Controller.setData(data);
 
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
@@ -42,5 +65,51 @@ public class Scene2Controller {
         stage.show();
     }
 
-   
+    void showAlert(AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.initOwner(stage);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+    void insertMovieIntoDatabase(String movieName) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.print("\n DRIVER ACTIVATED..... ");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            showAlert(AlertType.ERROR, "Database Error", "Database driver not found.");
+            return;
+        }
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviename", "root", "ayesha")) {
+            System.out.print("\n DATABASE CONNECTED.... ");
+            String insertQuery = "INSERT INTO movie (mn) VALUES (?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setString(1, movieName);
+            
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showAlert(AlertType.ERROR, "Database Error", "Failed to execute the database query.");
+        }
+        
+        
+        
+        try {
+        	 FXMLLoader loader2 = new FXMLLoader(getClass().getResource("Scene4.fxml"));
+			root = loader2.load();
+			Scene4Controller scene = loader2.getController();
+			scene.moviename.setText(movieName);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+         
+    }
 }
+
--- Scene4Controller.java
+++ Scene4Controller.java
@@ -1,15 +1,49 @@
 package application;
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class Scene4Controller {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Scene4Controller implements Initializable {
+    @FXML Label moviename;
+    
+
+    private Stage stage;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/moviename", "root", "ayesha")) {
+            String sql = "SELECT movie_name FROM movienames ORDER BY id ASC LIMIT 1"; // Use the correct table name and column name
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                String movieName = resultSet.getString("movie_name"); // Use the correct column name
+
+                // Set the movie name to the Label with fx:id "moviename"
+                moviename.setText(movieName);
+            }
+        } catch (SQLException e) {
+            // Handle the error, e.g., log it
+            showAlert(Alert.AlertType.ERROR, "Database Error", "Failed to retrieve movie name.");
+        }
+    }
 
     @FXML
     public void goBackToScene3(ActionEvent event) throws IOException {
@@ -18,11 +52,22 @@ public class Scene4Controller {
 
         Scene3Controller scene3Controller = loader.getController();
         // Pass any necessary data back to Scene3 if needed
-        // scene3Controller.setData(data);
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+    // Add handlers for the "book more" and "homepage" buttons if needed.
+
+    // Helper method to show an alert
+    private void showAlert(Alert.AlertType alertType, String title, String content) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
 }
+
